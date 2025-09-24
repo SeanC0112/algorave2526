1bbf99b38cceec6d89de62437f9a5427be5154b9
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2025-09-24 10:50:18 -0700

    first one wahoo

--- gibber.js
+++ gibber.js
@@ -1,1 +1,44 @@
 //put gibber code here, do we want to make multiple files for different instruments?
+Gibber.setBPM(100)
+
+// synth 
+
+s = Synth("cry")
+
+s.attack = 0.3
+s.decay = 0.4
+s.sustain = 0.7
+s.release = 1.3
+
+synthPattern1 = s.note.seq([0, 4, 7, 11, 9, 2, 5, 9], 1/4)
+
+//claude stuff kinda good
+bass = Synth('bass').note.seq([0,3,5,2], 1/4)
+bass.fx.add(Distortion())
+
+// Lead melody
+lead = Synth('lead')
+lead.note.seq([7,5,8,7,5,3,5,7], 1/8)
+lead.fx.add(Delay(1/8, 0.3))
+
+// rhythem  
+rhythm = Synth('square')
+rhythm.note.seq([0,0,2,2], 1/16)
+rhythm.gain = 0.3
+
+// Orchestral touches
+pad = Synth[4]('stringPad')
+pad.chord.seq([[0,2,4,7],[3,5,7,10]], 2)
+pad.gain = 0.4
+
+// drums
+
+drums = Drums()
+drums.tidal('[kd kd ~ kd] sd [~ sd ~ sd] ch*8')
+drums.tidal('[ch?0.3]*8')
+
+drums.tidal('<kd sd kd [oh,kd]> ch*2')
+
+kick = Kick()
+snare = Snare()
+hh = HiHat()
