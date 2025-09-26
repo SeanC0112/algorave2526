49271f619c4d858644ea4bcf9e24e9d0b5a0f388
sloane <sloanemvelazco@gmail.com>
2025-09-26 09:59:57 -0700

    Bus fix

--- gibber.js
+++ gibber.js
@@ -9,7 +9,7 @@ introChords = Freesound( 687041 )
 
 fadeIn = Bus()
 fadeIn.gain(0)
-backgroundBus.gain.fade(0.8, 4)
+fadeIn.gain.fade(0.8, 4)
 
 // BEGIN --------
 
@@ -20,7 +20,6 @@ introChords.trigger(1)
 rhythm = Synth('square')
 rhythm.note.seq([0,0,2,2], 1/16)
 rhythm.gain = 0.3
-rhythm.send(fadeIn, 1)
 
 // strings (1.5)
 pad = Synth[4]('stringPad')
@@ -30,17 +29,14 @@ pad.gain = 0.4
 
 // LAYERS ---------
 
-// bass (3)
+// claude stuff kinda good (3)
 bass = Synth('bass').note.seq([0,3,5,2], 1/4)
-bass.send(fadeIn, 1)
 bass.fx.add(Distortion())
 
 
 // melody (2)
 lead = Synth('lead')
 lead.note.seq([7,5,8,7,5,3,5,7], 1/8)
-lead.send(fadeIn, 1)
-
 
 // MISC ---------
   
@@ -51,9 +47,12 @@ synthEffect.trigger(1)
 
 // (1)
 drums = Drums()
-drums.tidal('[kd kd ~ kd] sd [~ sd ~ sd] ch*8', 0)
-drums.tidal('[ch?0.3]*8', 1)
-drums.tidal('<kd sd kd [oh,kd]> ch*2', 2)
+drums.tidal('[kd kd ~ kd] sd [~ sd ~ sd] ch*8')
+drums.tidal('[ch?0.3]*8')
+
+// (2)
+drums2 = Drums()
+drums2.tidal('<kd sd kd [oh,kd]> ch*2')
 
 
 
